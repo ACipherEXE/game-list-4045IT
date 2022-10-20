bf2d2f912cd5e55de8d3a7d9075e06b9a78d35b5
Christopher Negron <sonycar2000@outlook.com>
2022-10-20 14:41:38 -0400

    Initial commit

---
+++ src/test/java/com/gameList/gamelist/GameListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gameList.gamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
